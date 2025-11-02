10b7235e739d22a879a47b2a4e2ac51fc4ddc10b
NomuraTBM <minbloxe@gmail.com>
2025-11-03 01:06:25 +0800

    Minor tweaks on color and fill

--- draw/src/main/java/com/gabriel/draw/command/SetColorCommand.java
+++ draw/src/main/java/com/gabriel/draw/command/SetColorCommand.java
@@ -2,27 +2,56 @@ package com.gabriel.draw.command;
 
 import com.gabriel.drawfx.command.Command;
 import com.gabriel.drawfx.service.AppService;
+import com.gabriel.drawfx.model.Shape;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SetColorCommand implements Command {
     Color color;
     Color prevColor;
+    Map<Shape, Color> prevColors;
     AppService appService;
+    boolean affectsShapes;
+
     public SetColorCommand(AppService appService, Color color) {
         this.appService = appService;
         this.color = color;
+        this.prevColors = new HashMap<>();
+
+        this.prevColor = appService.getColor();
+        List<Shape> selectedShapes = appService.getSelectedShapes();
+        this.affectsShapes = !selectedShapes.isEmpty();
+
+        for (Shape shape : selectedShapes) {
+            prevColors.put(shape, shape.getColor());
+        }
     }
 
     @Override
     public void execute() {
-        prevColor = appService.getColor();
-        appService.setColor(color);
+        if (affectsShapes) {
+            for (Shape shape : prevColors.keySet()) {
+                shape.setColor(color);
+            }
+        }
+        else {
+            appService.getDrawing().setColor(color);
+        }
     }
 
     @Override
     public void undo() {
-        appService.setColor(prevColor);
+        if (affectsShapes) {
+            for (Map.Entry<Shape, Color> entry : prevColors.entrySet()) {
+                entry.getKey().setColor(entry.getValue());
+            }
+        }
+        else {
+            appService.getDrawing().setColor(prevColor);
+        }
     }
 
     @Override
--- draw/src/main/java/com/gabriel/draw/command/SetFillCommand.java
+++ draw/src/main/java/com/gabriel/draw/command/SetFillCommand.java
@@ -1,28 +1,55 @@
 package com.gabriel.draw.command;
 
 import com.gabriel.drawfx.command.Command;
+import com.gabriel.drawfx.model.Shape;
 import com.gabriel.drawfx.service.AppService;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SetFillCommand implements Command {
     Color fill;
     Color prevFill;
+    Map<Shape, Color> prevFills;
+    boolean affectsShapes;
     AppService appService;
+
     public SetFillCommand(AppService appService, Color fill) {
         this.appService = appService;
         this.fill = fill;
+        this.prevFills = new HashMap<>();
+
+        this.prevFill = appService.getFill();
+        List<Shape> selectedShapes = appService.getSelectedShapes();
+        this.affectsShapes = !selectedShapes.isEmpty();
+
+        for (Shape shape : selectedShapes) {
+            prevFills.put(shape, shape.getFill());
+        }
     }
 
     @Override
     public void execute() {
-        prevFill = appService.getFill();
-        appService.setFill(fill);
+        if (affectsShapes) {
+            for (Shape shape : prevFills.keySet()) {
+                shape.setFill(fill);
+            }
+        } else {
+            appService.getDrawing().setFill(fill);
+        }
     }
 
     @Override
     public void undo() {
-        appService.setFill(prevFill);
+        if (affectsShapes) {
+            for (Map.Entry<Shape, Color> entry : prevFills.entrySet()) {
+                entry.getKey().setFill(entry.getValue());
+            }
+        } else {
+            appService.getDrawing().setFill(prevFill);
+        }
     }
 
     @Override
